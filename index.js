0c30486644b92911721d6834930d6f3499eb77c4
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-06-03 22:47:12 +0600

    added get users

--- index.js
+++ index.js
@@ -60,6 +60,11 @@ async function run() {
         const user = req.body ;
         const result = await userCollection.insertOne(user);
         res.send(result) ;
+    }) ;
+    
+    app.get('/users', async(req,res)=> {
+      const result = await userCollection.find().toArray();
+      res.send(result) ;
     })
 
 
